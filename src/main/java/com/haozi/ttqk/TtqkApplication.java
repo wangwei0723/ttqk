5271211865264ea343c036677cebce7a5dd6911b
wangwei <wangwei1@able-elec>
2020-11-26 16:09:24 +0800

    init

---
+++ src/main/java/com/haozi/ttqk/TtqkApplication.java
@@ -0,0 +1,13 @@
+package com.haozi.ttqk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtqkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtqkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haozi/ttqk/TtqkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haozi.ttqk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtqkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
